537ef34b1a33973d6020ba7335e4a4daa8fdfa73
Aditya Kumar <95424749+Aditya06kr@users.noreply.github.com>
2022-06-07 21:10:13 +0530

    Solution
    Question Link - https://www.codechef.com/LTIME107D/problems/TRANSFORM

---
+++ April Lunchtime 2022 Division 4(Rated)/Mario and Transformation.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,count1=0,count2=1,count3=2;
+	    cin>>x;
+	    while(true){
+	        if(count1==x){
+	            cout<<"NORMAL\n";
+	            break;
+	        }
+	        else if(count2==x){
+	            cout<<"HUGE\n";
+	            break;
+	        }
+	        else if(count3==x){
+	            cout<<"SMALL\n";
+	            break;
+	        }
+	        count1+=3;
+	        count2+=3;
+	        count3+=3;
+	    }
+	}
+	return 0;
+}
